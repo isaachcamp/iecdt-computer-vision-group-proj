1b5456c6a372b01d6542be577cc6e1ce9faa38f5
Isaac Campbell <isaac.campbell@dtc.ox.ac.uk>
2025-02-27 17:13:46 +0000

    Add test set splitter

--- define_dataset.py
+++ define_dataset.py
@@ -4,6 +4,9 @@ import cv2
 from torch.utils.data import Dataset
 from torch.nn.functional import one_hot
 import torch
+from torch.utils.data import SubsetRandomSampler
+from torch.utils.data import DataLoader
+from torch.utils.data import random_split
 
 class CloudDataset(Dataset):
     def __init__(self, annotations_file, img_dir, transform, target_transform=None):
@@ -13,7 +16,8 @@ class CloudDataset(Dataset):
         self.target_transform = target_transform
 
     def __len__(self):
-        return len(self.img_labels)
+        # drop the heights column from the count
+        return len(self.img_labels.columns) - 1
 
     def __getitem__(self, idx: int):
         img_path = self.img_dir / f'{self.img_labels.columns[idx]}.png'
@@ -35,3 +39,14 @@ def target_transform_one_hot(target):
     """Convert target from pd.Series to one hot encoding of torch tensor"""
     target_arr = torch.from_numpy(target.values)
     return one_hot(target_arr, 2)
+
+def split_data(dataset, batch_size, train=0.7, val=0.2):
+    # seed for reproducibility to ensure we're getting the same dataset across evaluations
+    generator1 = torch.Generator().manual_seed(42)
+    random_splits = random_split(dataset, [train, val, 1-train-val], generator=generator1)
+
+    train_loader = DataLoader(random_splits[0], batch_size, shuffle=True)
+    val_loader = DataLoader(random_splits[1], batch_size)
+    test_loader = DataLoader(random_splits[2], batch_size)
+
+    return train_loader, val_loader, test_loader
